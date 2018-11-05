9926ffbb5767c48c4c9db9b84f90cc8bbe3ed0e9
Zhang Yong <zyos@qq.com>
2018-11-05 10:34:46 +0800

    新增for.sh 和 zhangyong.txt

---
+++ for.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#File Name    : for.sh
+#Author       : zhangyong
+#Mail         : zyos@qq.com
+#Create Time  : 2018-11-05 10:30
+#Description  : 
+
+for i in `seq 0 2 20`
+  do
+    echo $i
+done
